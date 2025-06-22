077e76574b8cd9d367e399405f588f82507b5963
noufalrahim <noufalrahim6784@gmail.com>
2025-06-23 01:25:50 +0530

    feat: added more origins for cors

--- src/main/java/com/tinysteps/tinysteps/config/CorsConfig.java
+++ src/main/java/com/tinysteps/tinysteps/config/CorsConfig.java
@@ -1,4 +1,5 @@
 package com.tinysteps.tinysteps.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,12 +12,12 @@ public class CorsConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+            public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://tinysteps-admin.vercel.app")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
+                        // Do NOT call .allowCredentials(true)
             }
         };
     }
